d08c24dcb609a9bb681125ce2fc3d2f698b92778
Steven Meyer <stanmeye@iu.edu>
2026-02-04 17:16:57 -0500

    Initial Commit

--- apputil.py
+++ apputil.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 
 # update/add code below ...
+def fibonacci(n):
+    """Return the nth number of the Fibonacci Series."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
